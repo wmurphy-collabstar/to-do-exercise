422bd8d9ec1b96c210310f3c840bb6d7985cd3cc
Winona Murphy <wviolinm@gmail.com>
2025-04-03 20:07:57 -0400

    add functionality to add task to list and mark it complete

--- todo.js
+++ todo.js
@@ -4,6 +4,24 @@ const list = document.getElementById('taskList');
 
 function createListItem(taskText){
     const listItem = document.createElement('li');
+    listItem.setAttribute("class", "item");
     listItem.textContent = taskText;
     return listItem;
 }
+
+function addTask(){
+    const taskText = task.value;
+    if (!taskText.trim()){return;}
+    if (list.innerText.toLowerCase().includes(taskText.toLowerCase())){ return;}
+    const listItem = createListItem(taskText);
+    list.append(listItem);
+    task.value = '';
+}
+
+function completeTask(event){
+    if (!event.target.classList.contains('item')){return;}
+    event.target.classList.toggle('completed');
+}
+
+addBtn.addEventListener("click", addTask);
+list.addEventListener("click", completeTask);
